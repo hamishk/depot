37f915d96b85a67a8f5dc6cf0e5c831c2726aa03
Hamish Kerr <user02@gmail.com>
2017-07-05 22:18:50 -0700

    move the index count initialization into the app controller to make action cable updates pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_index_view_count
+
+  private
+  # let's initialize the view counter at zero
+  def set_index_view_count
+    if session[:index_visit_count].nil?
+      session[:index_visit_count] = 0
+    end
+  end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,12 +3,5 @@ class StoreController < ApplicationController
   before_action :set_cart
   def index
     @products = Product.order(:title)
-
-    # log the visit to the index
-    if session[:index_visit_count].nil?
-      session[:index_visit_count] = 1
-    else
-      session[:index_visit_count] += 1
-    end
   end
 end
